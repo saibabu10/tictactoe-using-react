2f04cd33e546a9e74e9cdcde2f3f77f282dc4065
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-19 22:43:38 +0530

    Add.title of the project is added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="container">
+      <div className='container-title'>Tic Tac Toe Game</div>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
