e76baee01de6165e7e12816b43feb82f853838ad
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-20 07:11:20 +0530

    Add.css and extra buttons are added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,23 @@ function App() {
     <>
     <div className="container">
       <div className='container-title'>Tic Tac Toe Game</div>
+      <div className='game-board'>
+      <div>
       <button className='square'>X</button>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+      </div>
+      <div>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+      </div>
+      <div>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+      </div>
+      </div>
     </div>
     </>
   )
