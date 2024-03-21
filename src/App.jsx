ea82e936302d15d39a67a88ed541f5d704222c04
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-21 05:45:18 +0530

    Add.used Props to Values for the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import './App.css'
-
+function SquareComponent({value})
+{
+  return <button className='square'>{value}</button>
+}
 function App() {
-
-
   return (
     <>
     <div className="container">
       <div className='container-title'>Tic Tac Toe Game</div>
       <div className='game-board'>
       <div>
-      <button className='square'>X</button>
-      <button className='square'>X</button>
-      <button className='square'>X</button>
+      <SquareComponent value="1"/>
+<SquareComponent value={2}/>
+<SquareComponent value={3}/>
       </div>
       <div>
-      <button className='square'>X</button>
-      <button className='square'>X</button>
-      <button className='square'>X</button>
+      <SquareComponent value={4}/>
+      <SquareComponent value={5}/>
+      <SquareComponent value={6}/>
       </div>
       <div>
-      <button className='square'>X</button>
-      <button className='square'>X</button>
-      <button className='square'>X</button>
+      <SquareComponent value={7}/>
+      <SquareComponent value={8}/>
+      <SquareComponent value={9}/>
       </div>
       </div>
     </div>
