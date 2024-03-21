e39b8a8624dc223d17af9da70a0af8a7b660ac81
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-21 07:18:34 +0530

    ADD.states and props with function is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import './App.css'
 import {useState} from 'react';
-function SquareComponent()
+function SquareComponent({value,onSquareClick})
 {
-  const[value,setValue] = useState(null)
-  const handleClick=()=>{
-    setValue('X');
-   }
-   return <button className='square' onClick={handleClick}>{value}</button>
+   return <button className='square' onClick={onSquareClick}>{value}</button>
 }
 
 function App() {
+  const[squares,setSquares] = useState(Array(9).fill(null));
+  function handleClick(i){
+   const nextSquares = squares.slice();
+   nextSquares[i]='X'
+   setSquares(nextSquares);
+   }
   return (
     <>
     <div className="container">
       <div className='container-title'>Tic Tac Toe Game</div>
       <div className='game-board'>
       <div className='board-row'>
-      <SquareComponent/>
-<SquareComponent/>
-<SquareComponent/>
+      <SquareComponent value={squares[0]} onSquareClick={()=>handleClick(0)}/>
+<SquareComponent value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+<SquareComponent value={squares[2]} onSquareClick={()=>handleClick(2)}/>
       </div>
       <div className='board-row'>
-      <SquareComponent/>
-      <SquareComponent/>
-      <SquareComponent/>
+      <SquareComponent value={squares[3]} onSquareClick={()=>handleClick(3)}/>
+      <SquareComponent value={squares[4]} onSquareClick={()=>handleClick(4)}/>
+      <SquareComponent value={squares[5]} onSquareClick={()=>handleClick(5)}/>
       </div>
       <div className='board-row'>
-      <SquareComponent/>
-      <SquareComponent/>
-      <SquareComponent/>
+      <SquareComponent value={squares[6]} onSquareClick={()=>handleClick(6)}/>
+      <SquareComponent value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+      <SquareComponent value={squares[8]} onSquareClick={()=>handleClick(8)}/>
       </div>
       </div>
     </div>
