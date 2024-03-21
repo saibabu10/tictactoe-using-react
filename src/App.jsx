227f8f2f9fedb15cefc4c4fd069a6103659f7fd8
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-21 22:41:44 +0530

    Add.created the xIsNext useState to change the x and o terms

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,19 @@ function SquareComponent({value,onSquareClick})
 }
 
 function App() {
+  const[xIsNext,setXIsNext] = useState(true);
   const[squares,setSquares] = useState(Array(9).fill(null));
   function handleClick(i){
    const nextSquares = squares.slice();
+   if(xIsNext)
+   {
    nextSquares[i]='X'
+   }
+   else{
+    nextSquares[i]='O'
+   }
    setSquares(nextSquares);
+   setXIsNext(!xIsNext);
    }
   return (
     <>
